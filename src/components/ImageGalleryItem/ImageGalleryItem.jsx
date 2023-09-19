fa4b4c02c4b52542446289dd4d345f3da53ca6b4
Dima-kzk <dimakzk@yahoo.com>
2023-09-19 22:34:25 +0300

    add ImageGalleryItem

---
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -0,0 +1,12 @@
+import { Li } from './ImageGalleryItem.styled';
+
+export default function ImageGalleryItem(props) {
+  return (
+    <Li
+      className="gallery-item"
+      onClick={() => props.onClick(props.largeImageURL)}
+    >
+      <img src={props.src} alt="" />
+    </Li>
+  );
+}
