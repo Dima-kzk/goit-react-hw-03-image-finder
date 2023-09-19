0217332c5f23a7cceafb6a46b3588c2a184e2987
Dima-kzk <dimakzk@yahoo.com>
2023-09-19 22:30:37 +0300

    add Loader

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,17 @@
+import { ThreeDots } from 'react-loader-spinner';
+import { Loaderwrapper } from './Loader.styled';
+
+export default function Loader() {
+  return (
+    <ThreeDots
+      height="80"
+      width="80"
+      radius="9"
+      color="#14eee3"
+      ariaLabel="three-dots-loading"
+      wrapperStyle={{ display: 'inline-block', textAlign: 'center' }}
+      wrapperClassName=""
+      visible={true}
+    />
+  );
+}
