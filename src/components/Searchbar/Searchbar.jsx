bab86c107249f92510fc5cdf2e8435608b852537
Dima-kzk <dimakzk@yahoo.com>
2023-09-19 22:31:28 +0300

    add SwarchBar

---
+++ src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,42 @@
+import { Component } from 'react';
+import { Header } from './Searchbar.styled';
+import { ImSearch } from 'react-icons/im';
+
+export default class Searchbar extends Component {
+  state = {
+    query: '',
+  };
+
+  handleInputCange = event => {
+    this.setState({ query: event.currentTarget.value });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.props.onSubmit(this.state.query);
+    this.setState({ query: '' });
+  };
+
+  render() {
+    return (
+      <Header className="searchbar">
+        <form className="form" onSubmit={this.handleSubmit}>
+          <button type="submit" className="button">
+            <span className="button-label">Search</span>
+            <ImSearch />
+          </button>
+
+          <input
+            className="input"
+            type="text"
+            autoComplete="off"
+            autoFocus
+            placeholder="Search images and photos"
+            value={this.state.query}
+            onChange={this.handleInputCange}
+          />
+        </form>
+      </Header>
+    );
+  }
+}
